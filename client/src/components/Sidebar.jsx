ea65770450fdac0f3dfac12cf02ca63d4948bcdc
Peter-1990 <harip3279@gmail.com>
2025-07-03 14:16:29 +0530

    logo added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
+
 const Sidebar = () => {
 
     const navigate = useNavigate();
@@ -32,7 +33,7 @@ const Sidebar = () => {
               <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                   <h1>Create your first playlist</h1>
                   <p className='font-light'>it's easy we will help you</p>
-                  <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer'>Create Playlist</button>
+                  <button  className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer'>Create Playlist</button>
               </div>
                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
                   <h1>Let's findsome podcasts to follow</h1>
